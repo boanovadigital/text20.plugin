e63f5061d9a6fa003d796faf6053b9aad08b8bfd
Ralf Biedert <Ralf Biedert@localhost>
2010-06-17 14:29:22 +0200

    Minor fixes.

--- src/de/dfki/km/text20/browserplugin/browser/browseradapter/BrowserAdapter.java
+++ src/de/dfki/km/text20/browserplugin/browser/browseradapter/BrowserAdapter.java
@@ -17,7 +17,6 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
  * MA 02110-1301  USA
-
  * 
  */
 package de.dfki.km.text20.browserplugin.browser.browseradapter;
