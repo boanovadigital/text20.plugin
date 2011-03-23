5965939e73ef8b3e9dd1593cd4a38c90dab50a5e
Ralf Biedert <Ralf Biedert@localhost>
2011-03-23 11:40:15 +0100

    Made JS fixation listener report more information.

--- src/de/dfki/km/text20/browserplugin/services/mastergazehandler/impl/gazehandler/fixation/FixationHandler.java
+++ src/de/dfki/km/text20/browserplugin/services/mastergazehandler/impl/gazehandler/fixation/FixationHandler.java
@@ -1,4 +1,4 @@
- /*
+/*
  * FixationHandler.java
  *
  * Copyright (c) 2010, Ralf Biedert, DFKI. All rights reserved.
@@ -29,12 +29,13 @@ import de.dfki.km.text20.services.evaluators.gaze.GazeEvaluator;
 import de.dfki.km.text20.services.evaluators.gaze.listenertypes.fixation.FixationEvent;
 import de.dfki.km.text20.services.evaluators.gaze.listenertypes.fixation.FixationEventType;
 import de.dfki.km.text20.services.evaluators.gaze.listenertypes.fixation.FixationListener;
+import de.dfki.km.text20.services.evaluators.gaze.util.FixationUtil;
 import de.dfki.km.text20.services.pseudorenderer.CoordinatesType;
 import de.dfki.km.text20.services.pseudorenderer.PseudorendererStatus;
 
 /**
  * @author rb
- *
+ * 
  */
 public class FixationHandler extends AbstractGazeHandler {
 
@@ -46,36 +47,37 @@ public class FixationHandler extends AbstractGazeHandler {
         if (handler.size() == 0) return;
 
         // If the element is not visible, don't do anything
-        if (!this.pseudorenderer.getStatus().contains(PseudorendererStatus.VISIBLE))
+        if (!this.pseudorenderer.getStatus().contains(PseudorendererStatus.VISIBLE) || event.getType() == FixationEventType.FIXATION_CONTINUED)
             return;
 
-        // Has the fixation started?
-        if (event.getType() == FixationEventType.FIXATION_START) {
-            final Point dp = this.pseudorenderer.convertPoint(event.getFixation().getCenter(), CoordinatesType.SCREEN_BASED, CoordinatesType.DOCUMENT_BASED);
-            if (dp == null) return;
+        // Get arguments for the fixation listner
+        final FixationUtil fu = new FixationUtil(event.getFixation());
+        final Point p = this.pseudorenderer.convertPoint(event.getFixation().getCenter(), CoordinatesType.SCREEN_BASED, CoordinatesType.DOCUMENT_BASED);
+        final String mode = event.getType().name().toUpperCase();
+        final StringBuilder args = new StringBuilder();
 
-            // Execute all handler
-            for (final String h : handler) {
-                this.browserPlugin.executeJSFunction(h, dp.x, dp.y);
-            }
-        }
+        // In case we could not convert the fixation, don't do anything
+        if (p == null) return;
 
-        // Or ended?
-        if (event.getType() == FixationEventType.FIXATION_END) {
-            // Execute all handler
-            for (final String h : handler) {
-                this.browserPlugin.executeJSFunction(h, -1, -1);
-            }
+        // Assemble optional arguments
+        args.append("duration=" + fu.getFixationDuration());
+        args.append(",meanderivation=" + fu.getMeanCenterDerivation());
+
+        // Execute all handler
+        for (final String h : handler) {
+            this.browserPlugin.executeJSFunction(h, mode, p.x, p.y, args.toString());
         }
     }
 
-    /* (non-Javadoc)
-     * @see de.dfki.km.augmentedtext.browserplugin.services.mastergazehandler.impl.gazehandler.AbstractGazeHandler#registerToEvaluator(de.dfki.km.augmentedtext.services.gazeevaluator.GazeEvaluator)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see de.dfki.km.augmentedtext.browserplugin.services.mastergazehandler.impl.gazehandler.AbstractGazeHandler#
+     * registerToEvaluator(de.dfki.km.augmentedtext.services.gazeevaluator.GazeEvaluator)
      */
     @Override
     protected void registerToEvaluator(final GazeEvaluator evaluator) {
         evaluator.addEvaluationListener(new FixationListener() {
-
             @Override
             public void newEvaluationEvent(final FixationEvent event) {
                 handleEvent(event);
